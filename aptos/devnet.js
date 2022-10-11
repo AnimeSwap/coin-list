9eb5e7f108efd872e928e4e20f99fa84a0d485cd
Azard <azardf4yy@gmail.com>
2022-10-11 10:05:21 +0800

    CNAME (#2)
    * CNAME
    
    * change url
    
    * change url

--- aptos/devnet.js
+++ aptos/devnet.js
@@ -4,7 +4,7 @@
     decimals: 8,
     symbol: 'APT',
     name: 'Aptos',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/APT.svg',
+    logoURL: 'https://coinlist.animeswap.org/icons/APT.svg',
     projectURL: 'https://aptoslabs.com/',
   },
   {
@@ -12,7 +12,7 @@
     decimals: 8,
     symbol: 'USDT',
     name: 'Tether USD',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/USDT.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDT.webp',
     extensions: {
       stableUSD: true,
     },
@@ -22,21 +22,21 @@
     decimals: 8,
     symbol: 'BTC',
     name: 'Bitcoin',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/BTC.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/BTC.webp',
   },
   {
     address: '0x12d75d5bde2535789041cd380e832038da873a4ba86348ca891d374e1d0e15ab::staked_aptos_coin::StakedAptosCoin',
     decimals: 8,
     symbol: 'tAPT',
     name: 'Tortuga Staked APT',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/tAPT.svg',
+    logoURL: 'https://coinlist.animeswap.org/icons/tAPT.svg',
   },
   {
     address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_dai_coin::TestMintCoin',
     decimals: 8,
     symbol: 'ceDAI',
     name: 'Celer Dai Stablecoin',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/DAI.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/DAI.webp',
     extensions: {
       stableUSD: true,
     },
@@ -46,7 +46,7 @@
     decimals: 8,
     symbol: 'ceUSDC',
     name: 'Celer USD Coin',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/USDC.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDC.webp',
     extensions: {
       stableUSD: true,
     },
@@ -56,7 +56,7 @@
     decimals: 8,
     symbol: 'ceUSDT',
     name: 'Celer Tether USD',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/USDT.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDT.webp',
     extensions: {
       stableUSD: true,
     },
@@ -66,13 +66,13 @@
     decimals: 8,
     symbol: 'ceWBTC',
     name: 'Celer Wrapped Bitcoin',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/BTC.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/BTC.webp',
   },
   {
     address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_weth_coin::TestMintCoin',
     decimals: 8,
     symbol: 'ceWETH',
     name: 'Celer Wrapped Ethereum',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/WETH.png',
+    logoURL: 'https://coinlist.animeswap.org/icons/WETH.png',
   },
 ]
--- aptos/testnet.js
+++ aptos/testnet.js
@@ -4,7 +4,7 @@
     decimals: 8,
     symbol: 'APT',
     name: 'Aptos',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/APT.svg',
+    logoURL: 'https://coinlist.animeswap.org/icons/APT.svg',
     projectURL: 'https://aptoslabs.com/',
   },
   {
@@ -12,7 +12,7 @@
     decimals: 8,
     symbol: 'USDT',
     name: 'Tether USD',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/USDT.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDT.webp',
     extensions: {
       stableUSD: true,
     },
@@ -22,21 +22,21 @@
     decimals: 8,
     symbol: 'BTC',
     name: 'Bitcoin',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/BTC.webp',
+    logoURL: 'https://coinlist.animeswap.org/icons/BTC.webp',
   },
   {
     address: '0x12d75d5bde2535789041cd380e832038da873a4ba86348ca891d374e1d0e15ab::staked_aptos_coin::StakedAptosCoin',
     decimals: 8,
     symbol: 'tAPT',
     name: 'Tortuga Staked APT',
-    logoURL: 'https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/tAPT.svg',
+    logoURL: 'https://coinlist.animeswap.org/icons/tAPT.svg',
   },
   {
     address: '0xe4497a32bf4a9fd5601b27661aa0b933a923191bf403bd08669ab2468d43b379::move_coin::MoveCoin',
     decimals: 8,
     symbol: 'MOVE',
     name: 'BlueMove Coin',
-    logoURL: ['https://raw.githubusercontent.com/AnimeSwap/coin-list/main/icons/MOVE.svg'],
+    logoURL: ['https://coinlist.animeswap.org/icons/MOVE.svg'],
     projectURL: 'https://bluemove.net/',
   },
 ]
