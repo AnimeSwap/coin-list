688f66c28ea3b1f2bed2ee35ab08ca202f110779
Azard <azardf4yy@gmail.com>
2022-10-18 23:40:19 +0800

    mainnet

---
+++ aptos/mainnet.js
@@ -0,0 +1,10 @@
+[
+  {
+    address: '0x1::aptos_coin::AptosCoin',
+    decimals: 8,
+    symbol: 'APT',
+    name: 'Aptos',
+    logoURL: 'https://coinlist.animeswap.org/icons/APT.svg',
+    projectURL: 'https://aptoslabs.com/',
+  }
+]
