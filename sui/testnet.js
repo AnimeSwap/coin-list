4b0c003599d83f4d3c15d0d3cfed1e0ed1520f64
Azard <azardf4yy@gmail.com>
2023-04-21 12:14:48 +0800

    add sui coin-list

---
+++ sui/testnet.js
@@ -0,0 +1,26 @@
+[
+  {
+    address: '0x2::sui::SUI',
+    decimals: 9,
+    symbol: 'SUI',
+    name: 'SUI',
+    logoURL: ['https://coinlist.animeswap.org/icons/SUI.svg'],
+    projectURL: 'https://sui.io/',
+  },
+  {
+    address: '0x702a609dc22a429a14618c86147e99c711b00d15653083fe17cf16861cd3b97b::usdc::USDC',
+    decimals: 8,
+    symbol: 'USDC',
+    name: 'USDC',
+    logoURL: ['https://coinlist.animeswap.org/icons/USDC.webp'],
+    projectURL: 'https://sui.io/',
+  },
+  {
+    address: '0x702a609dc22a429a14618c86147e99c711b00d15653083fe17cf16861cd3b97b::usdt::USDT',
+    decimals: 8,
+    symbol: 'USDT',
+    name: 'USDT',
+    logoURL: ['https://coinlist.animeswap.org/icons/USDT.webp'],
+    projectURL: 'https://sui.io/',
+  },
+]
